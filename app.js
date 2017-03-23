3f6cf0bdb0d4f605a48d7793742ab415bc501a75
Carlos Carrera <carlos.carrera.altran@axa-groupsolutions.com>
2017-03-23 18:05:46 +0100

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var request = require('request');
+var qs = require('querystring');
 
 //=========================================================
 // Bot Setup
@@ -17,6 +19,10 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD || "py2hV5NjmZm2F9zhaRA55no"
 });
 
+
+
+
+
 /*
 App Id - cfa14f27-816e-40d6-82e6-606b3c3ed5db
 
@@ -24,6 +30,11 @@ App Password - py2hV5NjmZm2F9zhaRA55no
 */
 
 var bot = new builder.UniversalBot(connector);
+
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/e59f50e2-dd6e-4548-9b1a-728521bc2b09?subscription-key=6b8b9dc3e3284486b0284f4d7d8cb2b3&verbose=true&q=';
+bot.recognizer(new builder.LuisRecognizer(model));
+
+
 server.post('/api/messages', connector.listen());
 
 //=========================================================
@@ -34,7 +45,7 @@ bot.dialog('/', [
     function (session) {
         session.sendTyping();
         session.send('Hi');
-        builder.Prompts.choice(session, "How are you doing? :)", [":)",":("]);
+        builder.Prompts.choice(session, "How are you doing? :)", [":-)",":-("]);
     },
     function (session, results, next) {
         if (results.response) {
@@ -44,7 +55,7 @@ bot.dialog('/', [
                 session.send('We can make your day even better!');
             } else {
                 session.sendTyping();
-                session.send('I\'m sorry. We can make your day better!');
+                session.send('I\'m sorry to hear that. We can make your day better!');
             }
             session.sendTyping();
             session.send('I wanted to talk to you because your policy is going to expire!');
@@ -89,18 +100,110 @@ bot.dialog('/', [
                     .attachmentLayout(builder.AttachmentLayout.carousel)
                     .attachments(cards);
                 session.send(reply);
+                session.sendTyping();
+                builder.Prompts.text(session, "Do you like the offer?");
             }
+        }
+    },
+    function (session, results, next) {
+        if(results.response){
+            console.log(results.response);
+            var urlLuis = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/e59f50e2-dd6e-4548-9b1a-728521bc2b09?subscription-key=6b8b9dc3e3284486b0284f4d7d8cb2b3&verbose=true&"
+            var blabla = qs.stringify({q: results.response});
+            urlLuis = urlLuis + blabla; 
+            console.log(urlLuis);
+            request.get(urlLuis, function(err, body, response){
+                console.log(response);
+                response = JSON.parse(response);
+                if(response.topScoringIntent.intent === 'expensive-price') {
+                 session.sendTyping();
+                 session.send("Ok! Maybe this is more interesting for you.");
+                var cards = getCardsAttachments2();
+                 var reply = new builder.Message(session)
+                    .attachmentLayout(builder.AttachmentLayout.carousel)
+                    .attachments(cards);
+                session.send(reply);
+
+
+                }
+            })
         }
     }
 ]);
 
-
+/*
+{
+  "query": "expensive",
+  "topScoringIntent": {
+    "intent": "expensive-price",
+    "score": 0.998561442
+  },
+  "intents": [
+    {
+      "intent": "expensive-price",
+      "score": 0.998561442
+    },
+    {
+      "intent": "None",
+      "score": 0.374366134
+    }
+  ],
+  "entities": []
+}*/
 
 
 function getCardsAttachments(session) {
     return [
         new builder.HeroCard(session)
-            .title('Azure Storage')
+            .title('')
+            .subtitle('Offload the heavy lifting of data center management')
+            .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
+            .images([
+                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/storage/media/storage-introduction/storage-concepts.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+            ]),
+
+        new builder.ThumbnailCard(session)
+            .title('DocumentDB')
+            .subtitle('Blazing fast, planet-scale NoSQL')
+            .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
+            .images([
+                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/documentdb/media/documentdb-introduction/json-database-resources1.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
+            ]),
+
+        new builder.HeroCard(session)
+            .title('Azure Functions')
+            .subtitle('Process events with a serverless code architecture')
+            .text('An event-based serverless compute experience to accelerate your development. It can scale based on demand and you pay only for the resources you consume.')
+            .images([
+                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-5daae9212bb433ad0510fbfbff44121ac7c759adc284d7a43d60dbbf2358a07a/images/page/services/functions/01-develop.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
+            ]),
+
+        new builder.ThumbnailCard(session)
+            .title('Cognitive Services')
+            .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
+            .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
+            .images([
+                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-68b530dac63f0ccae8466a2610289af04bdc67ee0bfbc2d5e526b8efd10af05a/images/page/services/cognitive-services/cognitive-services.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/cognitive-services/', 'Learn More')
+            ])
+    ];
+}
+
+function getCardsAttachments2(session) {
+    return [
+        new builder.HeroCard(session)
+            .title('')
             .subtitle('Offload the heavy lifting of data center management')
             .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
             .images([
