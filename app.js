a5b9118506ed3bb152ac6dcbf54f1685b24bc59d
Carlos Carrera <carlos.carrera.altran@axa-groupsolutions.com>
2017-03-23 16:49:13 +0100

    first commit agaaain

--- app.js
+++ app.js
@@ -7,44 +7,140 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 1337, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID || "cfa14f27-816e-40d6-82e6-606b3c3ed5db",
+    appPassword: process.env.MICROSOFT_APP_PASSWORD || "py2hV5NjmZm2F9zhaRA55no"
 });
+
+/*
+App Id - cfa14f27-816e-40d6-82e6-606b3c3ed5db
+
+App Password - py2hV5NjmZm2F9zhaRA55no
+*/
+
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
-
+var msg;
 bot.dialog('/', [
     function (session) {
-        // Send a greeting and show help.
-        var card = new builder.HeroCard(session)
-            .title("Microsoft Bot Framework")
-            .text("Your bots - wherever your users are talking.")
-            .images([
-                 builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
+        session.sendTyping();
+        session.send('Hi');
+        builder.Prompts.choice(session, "How are you doing? :)", [":)",":("]);
+    },
+    function (session, results, next) {
+        if (results.response) {
+            console.log(results.response);
+            if(results.response.index === 0){
+                session.sendTyping();
+                session.send('We can make your day even better!');
+            } else {
+                session.sendTyping();
+                session.send('I\'m sorry. We can make your day better!');
+            }
+            session.sendTyping();
+            session.send('I wanted to talk to you because your policy is going to expire!');
+             msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([
+                new builder.HeroCard(session)
+                    .title("Your policy")
+                    .subtitle("")
+                    .text("ID: <b>1234</b> <br> Date: <b>2017-04-02</b> ")
+                    .images([
+                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                    ])
+                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
             ]);
-        var msg = new builder.Message(session).attachments([card]);
-        //session.send(msg);
-        session.send("Hi How are you doing? :) ");
-        session.send("");
-        session.send("")
-        session.beginDialog('/help');
+            session.sendTyping();
+            session.send(msg);
+            builder.Prompts.choice(session, "I hope you are happy with us, do you want to renew?", ["Yes", "No"]);
+        } else {
+            next();
+        }
     },
-    function (session, results) {
-        // Display menu
-        session.beginDialog('/menu');
+    function (session, results, next) {
+        if(results.response){
+            console.log(results.response);
+            if(results.response.index === 0) {
+                session.sendTyping();
+                session.send("That\'s great, at AXA, clients come first, because of that we have new offers just for you.");
+            } else {
+                builder.Prompts.choice(session, "We want to know what is the problem with the current policy?", ["Price is high", "Low coverage", "Others"]);
+                //session.send("In Axa clients come first, thats why we want to improve your current offer, adding some products to your package.")
+            }
+        } else next();
     },
-    function (session, results) {
-        // Always say goodbye
-        session.send("Ok... See you later!");
+    function (session, results, next) {
+        if(results.response){
+            if(results.response.index === 0) {
+                session.sendTyping();
+                session.send("OK, we can do a 30% discount in your price if you buy these products! ");
+                 var cards = getCardsAttachments();
+                 var reply = new builder.Message(session)
+                    .attachmentLayout(builder.AttachmentLayout.carousel)
+                    .attachments(cards);
+                session.send(reply);
+            }
+        }
     }
 ]);
+
+
+
+
+function getCardsAttachments(session) {
+    return [
+        new builder.HeroCard(session)
+            .title('Azure Storage')
+            .subtitle('Offload the heavy lifting of data center management')
+            .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
+            .images([
+                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/storage/media/storage-introduction/storage-concepts.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+            ]),
+
+        new builder.ThumbnailCard(session)
+            .title('DocumentDB')
+            .subtitle('Blazing fast, planet-scale NoSQL')
+            .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
+            .images([
+                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/documentdb/media/documentdb-introduction/json-database-resources1.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
+            ]),
+
+        new builder.HeroCard(session)
+            .title('Azure Functions')
+            .subtitle('Process events with a serverless code architecture')
+            .text('An event-based serverless compute experience to accelerate your development. It can scale based on demand and you pay only for the resources you consume.')
+            .images([
+                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-5daae9212bb433ad0510fbfbff44121ac7c759adc284d7a43d60dbbf2358a07a/images/page/services/functions/01-develop.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
+            ]),
+
+        new builder.ThumbnailCard(session)
+            .title('Cognitive Services')
+            .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
+            .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
+            .images([
+                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-68b530dac63f0ccae8466a2610289af04bdc67ee0bfbc2d5e526b8efd10af05a/images/page/services/cognitive-services/cognitive-services.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/cognitive-services/', 'Learn More')
+            ])
+    ];
+}
